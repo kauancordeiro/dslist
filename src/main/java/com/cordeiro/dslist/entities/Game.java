a5f4fe5f1f63d817df149df713b80d15abc6de46
kauancordeiro <cvitorcordeiro@gmail.com>
2023-10-02 23:10:10 -0300

    properties, game, orm

---
+++ src/main/java/com/cordeiro/dslist/entities/Game.java
@@ -0,0 +1,29 @@
+package com.cordeiro.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platform;
+    private String score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+
+}
